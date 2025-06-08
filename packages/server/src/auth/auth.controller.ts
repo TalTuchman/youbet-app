5ce030d3727e235a4ec7a92053a5333c08d4b81d
TalTuchman <taltuchman1@gmail.com>
2025-06-08 18:34:11 +0000

    add Get to Auth controller

--- packages/server/src/auth/auth.controller.ts
+++ packages/server/src/auth/auth.controller.ts
@@ -1,10 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpDto } from './dto/signup.dto';
+
 
 @Controller('auth') // All routes in this controller will start with /auth
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+  
+  @Get('hello')
+    getHello() {
+    return { message: 'Hello from YouBet!' };
+  }
 
   @Post('signup') // Creates a POST /auth/signup endpoint
   signup(@Body() dto: SignUpDto) {
