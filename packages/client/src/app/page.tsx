fed1b81a19daa8d70f1e7ec4a90c1187aad7c0ac
TalTuchman <taltuchman1@gmail.com>
2025-06-10 20:41:18 +0000

    build client UI

---
+++ packages/client/src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl font-bold">YouBet</h1>
+      <p className="mt-2 text-lg text-gray-400">Authentication Test Page</p>
+      {/* Our forms will go here later */}
+    </main>
+  );
+}
