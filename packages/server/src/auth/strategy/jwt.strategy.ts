24b4aa5be02e9e246624bb8eb1349c3d1d2bbcac
TalTuchman <taltuchman1@gmail.com>
2025-06-11 21:17:35 +0000

    jwt strategy

--- packages/server/src/auth/strategy/jwt.strategy.ts
+++ packages/server/src/auth/strategy/jwt.strategy.ts
@@ -3,25 +3,20 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PrismaService } from '../../prisma/prisma.service';
 
-// Read the secret at the top level.
 const jwtSecret = process.env.JWT_SECRET;
 
-// Fail-fast guard clause.
-// If the JWT_SECRET is not defined in the .env file, the entire application will fail to start.
-// This is better than failing at runtime when a user tries to log in.
 if (!jwtSecret) {
   throw new Error('JWT_SECRET is not defined in the environment variables');
 }
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  // We no longer need to inject ConfigService.
   constructor(private prisma: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      // Pass the guaranteed-to-exist constant.
-      secretOrKey: jwtSecret,
+      // Use the non-null assertion to satisfy the compiler
+      secretOrKey: jwtSecret!, 
     });
   }
 
@@ -33,9 +28,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     if (!user) {
       throw new UnauthorizedException();
     }
-    
-    // As decided, return the full user object.
-    // NestJS will attach this to the request (req.user).
+
     return user;
   }
 }
